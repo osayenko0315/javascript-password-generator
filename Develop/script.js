86c8a63552d68f83ec2e60b9e75fbbbe81cb426c
osayenko0315 <126353984+osayenko0315@users.noreply.github.com>
2023-03-16 21:20:45 -0500

    trying to add java

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,60 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+let userWantsNumber = window.confirm(
+  "Like to include numbersin password"
+);
+let userWantsSymbols = window.confirm(
+  "Like to include symbols in password"
+);
+let userWantsLowerCase = window.confirm(
+  "Like to include lower case in password"
+);
+let userWantsUpperCase = window.confirm(
+  "Like to include upper case in password"
+);
+
+let numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let symbolsList = ["!", "@", "#", "$", "%", "^", "&", "*"];
+let lowerCaseList = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+let upperCaseList = []
+
+
+function writePassword()
+let password = generatePassword();
+let passwordText = document.querySelector("#password")
+
+if (password) {
+  passwordText.value = password
+}
+
+generateButton.addEventListener("click" writePassword);
